d1a6971df13d41f10e5a16345b276b730d79032c
sujin <ikosdu60@gmail.com>
2023-02-28 16:16:40 +0900

    feat: 클릭된 StudyNav Redux Store에 저장

--- src/components/study/StudyNavigationBar.tsx
+++ src/components/study/StudyNavigationBar.tsx
@@ -1,12 +1,25 @@
+import { useDispatch } from 'react-redux'
+import { useAppSelector } from 'src/lib/hooks'
+import { changeTargetedStudyNav } from 'src/lib/store/studyItemSlice'
 import styled from 'styled-components'
 
 const StudyNavigationBar = () => {
-  const test = 'test'
+  const dispatch = useDispatch()
+  const { clickedStudyNav } = useAppSelector(({ studyItem }) => studyItem)
+  const onClickNavMenu = (e: React.MouseEvent<HTMLDivElement>) => {
+    dispatch(changeTargetedStudyNav(e.currentTarget.id))
+  }
   return (
     <Container>
-      <NavMenu>Home</NavMenu>
-      <NavMenu>게시판</NavMenu>
-      <NavMenu>설정</NavMenu>
+      <NavMenu id="home" onClick={onClickNavMenu} clickedStudyNav={clickedStudyNav === 'home'}>
+        Home
+      </NavMenu>
+      <NavMenu id="board" onClick={onClickNavMenu} clickedStudyNav={clickedStudyNav === 'board'}>
+        게시판
+      </NavMenu>
+      <NavMenu id="settings" onClick={onClickNavMenu} clickedStudyNav={clickedStudyNav === 'settings'}>
+        설정
+      </NavMenu>
     </Container>
   )
 }
@@ -19,16 +32,18 @@ const Container = styled.div`
   margin: 4.5rem 0rem;
   padding-left: 2rem;
 
-  font-size: 1.5rem;
+  font-size: 1.2rem;
 `
-const NavMenu = styled.nav`
+const NavMenu = styled.nav<{ clickedStudyNav: boolean }>`
   margin-right: 3rem;
   padding: 0rem 1rem;
   line-height: 3rem;
+  font-weight: bold;
+  color: ${(props) => (props.clickedStudyNav ? props.theme.text1 : props.theme.text4)};
   border-top-left-radius: 20%;
   border-top-right-radius: 20%;
-
-  background: ${(props) => props.theme.bgGroundColor2};
+  transition: box-shadow 300ms ease-in-out, 300ms ease-in-out;
+  background: ${(props) => (props.clickedStudyNav ? props.theme.bgGroundColor2 : 'none')};
 
   :hover {
     cursor: pointer;
