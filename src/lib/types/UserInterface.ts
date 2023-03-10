cea70d33426357435e7cfa4c9c3564f50e01b2f9
sujin <ikosdu60@gmail.com>
2023-03-10 14:39:53 +0900

    refactor: UserInterface 중복 부분 상속처리

--- src/lib/types/UserInterface.ts
+++ src/lib/types/UserInterface.ts
@@ -1,28 +1,29 @@
-export interface ValidationParam {
-  username: boolean
-  email: boolean
-  password: boolean
-}
-
-export interface UserParam {
-  imageFile: File | null | string
-  username: string
-  email: string
-  password: string
-}
-
 export interface SignInParam {
   email: string
   password: string
 }
 
-export interface EditMemberParam {
-  profileUrl: string | null
-  imageFile: File | null
+export interface ValidationParam extends SignInParam {
   username: string
+}
+
+export interface UserParam extends ValidationParam {
+  imageFile: File | null | string
+}
+
+export interface MemberFormParam extends UserParam {
+  profileUrl: string | null
   imageUrl: string
 }
-export interface UserInfoInterface {
+
+export interface ParticipateParam {
+  memberId: number
+  username: string
+  imageUrl: string
+  profileUrl: string | null
+  participationDate: string
+}
+export interface MemberInfoInterface {
   [key: string]: string | number | null
   imageUrl: string
   profileUrl: string
@@ -32,12 +33,4 @@ export interface UserInfoInterface {
   joinedAt: string
   memberId: number
   role: string
-}
-
-export interface ParticipateParam {
-  memberId: number
-  username: string
-  imageUrl: string
-  profileUrl: string | null
-  participationDate: string
 }
