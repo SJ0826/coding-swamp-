c81066c943fc01ba1a24ba2046efa583b5425dc0
sujin <ikosdu60@gmail.com>
2023-04-04 23:29:40 +0900

    chore: package.json 삭제 후 재설치

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,5 @@
 export { default as DividingLine } from './DividingLine'
 export { default as DefaultButton } from './DefaultButton'
+
+// eslint-disable-next-line no-console
+console.log('testing')
